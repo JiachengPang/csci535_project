78cf225117cc1f324874b5a584cb2b27a6286bdf
Jiacheng pang <andypjc7529@outlook.com>
2025-04-25 23:21:34 -0700

    xnorm fix and decoder

--- decoder.py
+++ decoder.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import LlamaForCausalLM, AutoTokenizer
 
@@ -16,7 +17,7 @@ class ProjectionLayer(nn.Module):
     x = x.view(x.size(0), self.prefix_len, -1)
 
 class MultimodalLLamaDecoder(nn.Module):
-  def __init__(self, llama_name='meta-llama/Llama-2-7b-chat-hf', encoder_dim=1536, llama_dim=4096, prefix_len=10):
+  def __init__(self, llama_name='huggyllama/llama-7b', encoder_dim=1536, llama_dim=4096, prefix_len=10):
     super().__init__()
     self.llama = LlamaForCausalLM.from_pretrained(llama_name)
     self.tokenizer = AutoTokenizer.from_pretrained(llama_name)
@@ -25,12 +26,45 @@ class MultimodalLLamaDecoder(nn.Module):
 
   def forward(self, features, input_ids=None, attention_mask=None, labels=None):
     batch_size = features.size(0)
-    
+    prefix_emb = self.projection(features)
+    token_emb = self.llama.embed_tokens(input_ids)
+    full_embeddings = torch.cat([prefix_emb, token_emb], dim=1)
+
+    if attention_mask is not None:
+      prefix_attention = torch.ones((batch_size, self.projection.prefix_len), dtype=attention_mask.dtype, device=attention_mask.device)
+      attention_mask = torch.cat([prefix_attention, attention_mask], dim=1)
+
+    # Feed into LLaMA
+    outputs = self.llama(
+      inputs_embeds=full_embeddings,
+      attention_mask=attention_mask,
+      labels=labels,
+      return_dict=True,
+    )
+
+    return outputs
+  
+  def generate(self, features, max_new_tokens=50):
+    batch_size = features.size(0)
     prefix_emb = self.projection(features)
 
-    token_emb = self.llama
+    start_tokens = torch.full(
+        (batch_size, 1),
+        self.tokenizer.bos_token_id,
+        dtype=torch.long,
+        device=features.device,
+    )
 
-if __name__ == '__main__':
-  llama_name="huggyllama/llama-7b"
-  llama = LlamaForCausalLM.from_pretrained(llama_name)
-  print(llama)
+    # get BOS and append after prefix
+    start_emb = self.llama.model.embed_tokens(start_tokens)
+    full_embeddings = torch.cat([prefix_emb, start_emb], dim=1)
+    attention_mask = torch.ones(full_embeddings.size()[:2], device=features.device)
+
+    outputs = self.llama.generate(
+        inputs_embeds=full_embeddings,
+        attention_mask=attention_mask,
+        max_new_tokens=max_new_tokens,
+        eos_token_id=self.tokenizer.eos_token_id,
+    )
+
+    return outputs
